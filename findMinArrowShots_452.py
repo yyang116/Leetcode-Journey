b975c08b3d448bfab2d8eb1f71e7323c5509a5c4
yyang116 <81680400+yyang116@users.noreply.github.com>
2021-09-21 10:56:46 +0200

    Create findMinArrowShots_452.py

---
+++ findMinArrowShots_452.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        points.sort(key=lambda x:x[1])
+        counter=1
+        right=points[0][1]
+        for i in range(1,len(points)):
+            if(points[i][0]>right):
+                counter+=1
+                right=points[i][1]
+        return counter
